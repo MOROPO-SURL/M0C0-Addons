b1194f9ad9bad8ef0fadedd55b53e41e98f38ca0
Miguel Angel Torres Pérez <miguel.torres.ss24@gmail.com>
2022-12-05 11:09:57 -0500

    Incorporando cambios para solucionar errores en instalación (3)

--- m0c0_account_reports/__manifest__.py
+++ m0c0_account_reports/__manifest__.py
@@ -9,14 +9,14 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Localization',
-    'version': '15.0.0.16',
+    'version': '15.0.0.17',
     # any module necessary for this one to work correctly
     'depends': ['account_financial_report'],
     # always loaded
     'data': [
         #'views/bank_reconcile_dashboard_view.xml',
         'views/bank_reconcile_report_template.xml',
-        'views/bank_reconcile_report_wizard_view.xml'
+        'reports/bank_reconcile_report_view.xml'
     ],
     'license': 'LGPL-3',
 }
