b451c36ad1674abd8a20bf84d5e2473b144fa64c
Miguel Angel Torres Pérez <miguel.torres.ss24@gmail.com>
2022-12-08 16:14:32 -0500

    Incorporando ajustes en la posición en el menú

--- m0c0_account_reports/__manifest__.py
+++ m0c0_account_reports/__manifest__.py
@@ -9,9 +9,9 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Localization',
-    'version': '15.0.0.32',
+    'version': '15.0.0.33',
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account_financial_report'],
+    'depends': ['base', 'account_financial_report', 'l10n_cu_reports'],
     # always loaded
     'data': [
         #'views/bank_reconcile_dashboard_view.xml',
