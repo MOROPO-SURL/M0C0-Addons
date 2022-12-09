5b9f29870313f8cead3f412e8e42820d74127da9
Miguel Angel Torres Pérez <miguel.torres.ss24@gmail.com>
2022-12-08 20:42:03 -0500

    Realizando ajustes para generar el reporte (2)

--- m0c0_account_reports/__manifest__.py
+++ m0c0_account_reports/__manifest__.py
@@ -5,17 +5,14 @@
     'description': 'Incluyendo reportes requeridos según las normas cubanas para la localización cubana de Odoo M0C0.',
     'author': 'MOROPO S.U.R.L.',
     'website': 'https://www.moropo.cu',
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Localization',
-    'version': '15.0.0.45',
+    'version': '15.0.0.46',
     # any module necessary for this one to work correctly
     'depends': ['base', 'account_financial_report', 'l10n_cu_reports'],
     # always loaded
     'data': [
         #'views/bank_reconcile_dashboard_view.xml',
-        'views/bank_reconcile_report_template.xml',
+        'report/bank_reconcile_report_template.xml',
         'wizard/bank_reconcile_report_wizard_views.xml',
         'security/ir.model.access.csv'
     ],
