c77f09526d2ef50fd052f0ed4adb0c5a4b4b5609
Miguel Angel Torres Pérez <miguel.torres.ss24@gmail.com>
2022-12-10 18:41:59 -0500

    Incorporando alerta de respaldo en los cierres (4)

--- m0c0_account_overrides/__manifest__.py
+++ m0c0_account_overrides/__manifest__.py
@@ -9,7 +9,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Localization',
-    'version': '15.0.0.13',
+    'version': '15.0.0.14',
     # any module necessary for this one to work correctly
     'depends': ['base','account','om_fiscal_year'],
     # always loaded
