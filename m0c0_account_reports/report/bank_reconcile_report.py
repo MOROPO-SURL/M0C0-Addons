5873a4d6e5be8bdd681a97135562292a1a99e8b6
Miguel Angel Torres Pérez <miguel.torres.ss24@gmail.com>
2022-12-11 18:05:03 -0500

    Solucionando errores en conciliación bancaria

--- m0c0_account_reports/report/bank_reconcile_report.py
+++ m0c0_account_reports/report/bank_reconcile_report.py
@@ -138,6 +138,55 @@ class BankReconcileReport(models.AbstractModel):
             subtotal += m['amount']
         return subtotal
 
+    def _get_accounts(self, accounts, display_account):
+        """ compute the balance, debit and credit for the provided accounts
+            :Arguments:
+                `accounts`: list of accounts record,
+                `display_account`: it's used to display either all accounts or those accounts which balance is > 0
+            :Returns a list of dictionary of Accounts with following key and value
+                `name`: Account name,
+                `code`: Account code,
+                `credit`: total amount of credit,
+                `debit`: total amount of debit,
+                `balance`: total amount of balance,
+        """
+
+        account_result = {}
+        # Prepare sql query base on selected parameters from wizard
+        tables, where_clause, where_params = self.env['account.move.line']._query_get()
+        tables = tables.replace('"','')
+        if not tables:
+            tables = 'account_move_line'
+        wheres = [""]
+        if where_clause.strip():
+            wheres.append(where_clause.strip())
+        filters = " AND ".join(wheres)
+        # compute the balance, debit and credit for the provided accounts
+        request = ("SELECT account_id AS id, SUM(debit) AS debit, SUM(credit) AS credit, (SUM(debit) - SUM(credit)) AS balance" +\
+                   " FROM " + tables + " WHERE account_id IN %s " + filters + " GROUP BY account_id")
+        params = (tuple(accounts.ids),) + tuple(where_params)
+        self.env.cr.execute(request, params)
+        for row in self.env.cr.dictfetchall():
+            account_result[row.pop('id')] = row
+
+        account_res = []
+        for account in accounts:
+            res = dict((fn, 0.0) for fn in ['credit', 'debit', 'balance'])
+            currency = account.currency_id and account.currency_id or account.company_id.currency_id
+            res['code'] = account.code
+            res['name'] = account.name
+            if account.id in account_result:
+                res['debit'] = account_result[account.id].get('debit')
+                res['credit'] = account_result[account.id].get('credit')
+                res['balance'] = account_result[account.id].get('balance')
+            if display_account == 'all':
+                account_res.append(res)
+            if display_account == 'not_zero' and not currency.is_zero(res['balance']):
+                account_res.append(res)
+            if display_account == 'movement' and (not currency.is_zero(res['debit']) or not currency.is_zero(res['credit'])):
+                account_res.append(res)
+        return account_res
+
     @api.model
     def _get_report_values(self, docids, data=None):
         journal_id = self.env['account.journal'].browse(data['form']['journal_id'])
@@ -151,7 +200,7 @@ class BankReconcileReport(models.AbstractModel):
         line_amount = 0
         line_type = ''
 
-        balance = self.env['accounting_pdf_reports.action_report_trial_balance'].with_context({'date_to': reconcile_date})._get_accounts(journal_id.default_credit_account_id, 'all')
+        balance = _get_accounts(journal_id.default_credit_account_id, 'all')
 
         for acc in balance:
             account_balance = abs(self._compute_currency(journal_id, reconcile_date, acc['balance']))
