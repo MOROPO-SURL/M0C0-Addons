e390afc82286af39fdffb782a917af5ff25ea4b1
Miguel Angel Torres Pérez <miguel.torres.ss24@gmail.com>
2022-12-10 21:21:18 -0500

    Incorporando comportamiento al botón de Salva (Retry)

--- m0c0_account_overrides/models/m0c0_change_lock_date.py
+++ m0c0_account_overrides/models/m0c0_change_lock_date.py
@@ -1,8 +1,8 @@
-from odoo import models, _
-from odoo.exceptions import UserError
+from odoo import models, api, _
 
 class M0C0ChangeLockDate(models.Model):
     _inherit = "change.lock.date"
 
+    @api.multi
     def button_perform_backup(self):
         self.env.ref('auto_backup.action_server_backup').run()
