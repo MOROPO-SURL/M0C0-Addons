e3741fcee94700aa86936c1aff3e9716df65a334
Miguel Angel Torres Pérez <miguel.torres.ss24@gmail.com>
2023-02-09 21:56:04 -0500

    Solucionando problema en la instalación (16)

--- Resilient/m0c0_account_accounting/__manifest__.py
+++ Resilient/m0c0_account_accounting/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "M0C0 - Contabilidad Financiera y Analítica",
-    "version": "0.1.15",
+    "version": "0.1.16",
     "author": "MOROPO S.U.R.L.",
     "website": "https://www.moropo.cu",
     "contact": "contacto@moropo.cu",
