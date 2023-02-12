a04f17163a857aaeddfe429b6ce24ee3be24de0b
Miguel Angel Torres Pérez <miguel.torres.ss24@gmail.com>
2023-02-11 21:52:07 -0500

    Incorporando cambios menores para lograr instalación

---
+++ Resilient/l10n_cu/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# Part of Desoft. See LICENSE file for full copyright and licensing details.
+
--- Resilient/l10n_cu/__manifest__.py
+++ Resilient/l10n_cu/__manifest__.py
@@ -7,13 +7,14 @@
 # Yusnel Rojas Garcia
 # Julio Smith
 # Segu
+# Miguel Angel Torres Pérez <matorres@moropo.cu>
 
 {
-    'name': 'M0C0 - Localización de Contabilidad para Cuba',
-    'version': '0.4.2',
-    'author': 'Idola Odoo Team, Comunidad cubana de Odoo, MOROPO S.U.R.L.',
-    'category': 'Accounting/Localizations/Account Charts',
-    'description': """
+    "name": "M0C0 - Localización de Contabilidad para Cuba",
+    "version": "0.5.0",
+    "author": "Idola Odoo Team, Comunidad cubana de Odoo, MOROPO S.U.R.L.",
+    "category": "Accounting/Localizations/Account Charts",
+    "description": """
         Cuban charts of accounts.
             * Defines the following chart of account templates:
                 * Cuban general chart of accounts by 494/2016 modified by 407/2019
@@ -21,20 +22,20 @@
                 * Cuban general chart of accounts for Unidades Presupuestadas de Tratamiento Especial 
                 * Cuban general chart of accounts for Sector Cooperativo Agropecuario y no Agropecuario"
     """,
-    'depends': [
-        'account',
+    "depends": [
+        "account",
     ],
-    'data': [
-        'data/account_chart_data.xml',
-        'data/account.account.template-tcp.csv',
-        'data/account.account.template-common.csv',
-        'data/account.account.template-private.csv',
-        'data/account.account.template-public.csv',
-        'data/account_chart_post_data.xml',
-        'data/account_tax_template_data.xml',
-        'data/account_fiscal_position_template_data.xml',
-        'data/account_fiscal_position_tax_template_data.xml',
-        'data/account_chart_template_data.xml',
+    "data": [
+        "data/account_chart_data.xml",
+        "data/account.account.template-tcp.csv",
+        "data/account.account.template-common.csv",
+        "data/account.account.template-private.csv",
+        "data/account.account.template-public.csv",
+        "data/account_chart_post_data.xml",
+        "data/account_tax_template_data.xml",
+        "data/account_fiscal_position_template_data.xml",
+        "data/account_fiscal_position_tax_template_data.xml",
+        "data/account_chart_template_data.xml",
     ],
-    'license': 'LGPL-3',
+    "license": "LGPL-3",
 }
