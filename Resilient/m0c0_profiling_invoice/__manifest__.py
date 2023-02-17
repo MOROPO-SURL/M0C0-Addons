8312a01118a73aad358f823dcf3b14a189485e73
Miguel Angel Torres Pérez <miguel.torres.ss24@gmail.com>
2023-02-16 23:45:49 -0500

    Incorporando módulo de perfilado de facturas

---
+++ Resilient/m0c0_profiling_invoice/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'M0C0 - Perfilado de Facturas',
+    'summary': 'Homologación de Facturas dentro de la cintabilidad de M0C0',
+    'description': 'Homologación de facturas de ventas dentro de la cintabilidad de M0C0 a las normas cubanas para la localización cubana de Odoo M0C0.',
+    'author': 'MOROPO S.U.R.L.',
+    'website': 'https://www.moropo.cu',
+    'category': 'Localization',
+    'version': '0.2.0,
+    'depends': ['account'],
+    'data': [
+        'views/account_invoice_globals.xml',
+        'views/account_invoice_signblocks.xml'
+    ],
+    'license': 'LGPL-3',
+}
