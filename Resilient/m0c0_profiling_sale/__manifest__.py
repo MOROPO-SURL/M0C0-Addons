e013e2441ec2a3a5abd2e381faf45f72ad3f0647
Miguel Angel Torres Pérez <miguel.torres.ss24@gmail.com>
2023-06-06 01:17:45 -0400

    Incorporando módulo para perfilado de presupuestos

---
+++ Resilient/m0c0_profiling_sale/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'M0C0 - Perfilado de Ventas',
+    'summary': 'Personalizaciones del del módulo de Ventas de M0C0',
+    'description': 'Perfilado de documentos para ventas para la localización cubana de Odoo M0C0.',
+    'author': 'MOROPO S.U.R.L.',
+    'website': 'https://www.moropo.cu',
+    'category': 'Localization',
+    'version': '0.1.8',
+    'depends': ['m0c0_account'],
+    'data': [
+        'views/account_budget_signblocks.xml'
+    ],
+    'license': 'LGPL-3',
+}
